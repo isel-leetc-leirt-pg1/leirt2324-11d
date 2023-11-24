c74fa3e6924556ee1889c244fbd32450f8100941
jmartins3 <jmartins@isel.pt>
2023-11-24 14:24:05 +0000

    pglib intro

--- aula-11-22/hello_graph.c
+++ aula-11-22/hello_graph.c
@@ -1,5 +1,13 @@
 #include <pg/pglib.h>
 
+
+/**
+ * programa exemplo de utilização da biblioteca gráfica
+ * 
+ * build: gcc -o hello_graph -Wall hello_graph.c -lpg
+ *
+ */
+ 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
